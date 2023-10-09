b42fd344130cf821d93538d49f8a7b84c4ce17d2
Peter Bachman <57814978+PeterBachman100@users.noreply.github.com>
2023-10-09 16:23:50 -0700

    Added image modals

--- tiny-habits.js
+++ tiny-habits.js
@@ -47,4 +47,34 @@ jQ(document).ready(function() {
 		}
 	});
 	
+	
+	
+	// Get the modal
+	var imageModal = document.getElementById("image-modal");
+	
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+	
+	// Get the button that opens the modal
+document.querySelectorAll('.zoom').forEach(item => {
+	  item.addEventListener('click', event => {
+		imageModal.style.display = "flex";
+		imageModal.children[0].children[1].src = item.src;
+	  })
+	})	
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	  imageModal.style.display = "none";
+	}
+	
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	  if (event.target == imageModal) {
+		imageModal.style.display = "none";
+	  }
+	}
+	
+	
+	
 });
